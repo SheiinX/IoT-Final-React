4bf8607a6deda1b3cee9562b1a6ce1ef934d4073
SheiinX <sebastianfrancocatano@gmail.com>
2025-05-29 09:04:07 -0500

    Initial commit working app flask react

---
+++ back-flask/back_end/config.py
@@ -0,0 +1,6 @@
+SECRET_KEY = 'una_clave_secreta_segura'
+SESSION_TYPE = 'filesystem'
+SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+#CONFIGURACION URL ORION
+ORION_URL = 'http://54.158.237.74:1026/v2/entities'
